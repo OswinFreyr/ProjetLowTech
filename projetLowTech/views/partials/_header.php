f4c82c804346449e8b2c08314b52b8abcb696fd3
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-22 09:55:54 +0100

    Finition header
    J'ai laissé le form en commentaire pour pouvoir le réutiliser sur la page de création de compte plus tard.

--- projetLowTech/views/partials/_header.php
+++ projetLowTech/views/partials/_header.php
@@ -7,6 +7,23 @@
         <li id="partenaires"> <a href="">Partenaires</a> </li>
         <li id="annonces"> <a href="">Annonces</a> </li>
         <li id="benevoles"> <a href="">Bénévoles</a> </li>
-        <li id="connexion">  </li>
+        <li id="connexion"> 
+            <a href="">Se connecter</a>
+            <a href="">S'inscrire</a>
+            <a href="">Mon compte</a>
+            <!-- <form method="POST">  A METTRE SUR LA PAGE D'INSCRIPTION
+                <label for="name">Nom :</label><br>
+                <input type="text" id="name" name="name" required><br>
+                <label for="password">Mot de passe :</label><br>
+                <input type="password" id="password" name="password" required><br>
+                <label for="address">Adresse :</label><br>
+                <input type="text" id="address" name="address" required><br>
+                <label for="email">Email :</label><br>
+                <input type="email" id="email" name="email" required><br>
+                <label for="tel">Téléphone :</label><br>
+                <input type="text" id="tel" name="tel" required><br>
+                <input type="submit" value="Créer un compte">
+            </form>  -->
+        </li>
     </ul>
 </header>
