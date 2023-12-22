074da3d2c2be90c1b913da7d76b0c87a9ce8aec5
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-22 11:48:03 +0100

    Ajout page partenaires
    /!\ Pas de route ni controller ajouté

---
+++ projetLowTech/views/pages/partenaires.php
@@ -0,0 +1,48 @@
+<div>
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Partenaire 1</h2>
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, quod adipisci esse, quos excepturi eum, laudantium veniam optio natus tempora consectetur dolorum? Quaerat blanditiis deserunt ipsa sed. Officiis, mollitia animi!
+    </div>
+    <div>
+        <div>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates eum magnam tempore doloremque eveniet maxime ipsam sint repellat vitae aut. Officiis molestias rem dolores, aut eius facere. Molestias, quisquam laborum?
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+    <div>
+        <button>
+            <a href="">Site</a>
+        </button>
+    </div>
+</div>
+<div>
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+        <h2>Partenaire 2</h2>
+    </div>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsum odit, repudiandae mollitia quo, sint vel minima dolore quas, non aut sit. Nobis repellendus quaerat veniam vel veritatis eum neque.
+    </div>
+    <div>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, praesentium dicta corporis, corrupti tempore provident ut perspiciatis recusandae dolor facilis molestiae quas culpa ullam quos? Corporis aliquid nam voluptatibus quod!
+        </div>
+    </div>
+    <div>
+        <button>
+            <a href="">Site</a>
+        </button>
+    </div>
+</div>
