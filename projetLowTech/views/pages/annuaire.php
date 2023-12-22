4b0ba97e72c3c648ab7d1a704b7c41fd3b16cb7e
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-22 12:39:09 +0100

    ajout pages carte profil et annuaire
    Pas de controller ni manager

---
+++ projetLowTech/views/pages/annuaire.php
@@ -0,0 +1,5 @@
+<div>
+    <div>Barre de recherche</div>
+    <div>Filtres</div>
+    <div>Cartes profil</div>
+</div>
---
+++ projetLowTech/views/pages/carteProfil.php
@@ -0,0 +1,24 @@
+<div>
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+        Nom Prénom
+    </div>
+    <div>
+        NomUtilisateur
+    </div>
+    <div>
+        <ul>
+            liste des compétences
+        </ul>
+    </div>
+    <div>
+        <div>
+            Ville
+        </div>
+        <div>
+            <button>Voir profil</button>
+        </div>
+    </div>
+</div>
