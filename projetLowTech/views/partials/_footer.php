baf02014e467c1e356616c733aaebe83fbd103b9
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2023-12-22 09:38:50 +0100

    Ajout titre dans footer
    j'avais oublié :(

--- projetLowTech/views/partials/_footer.php
+++ projetLowTech/views/partials/_footer.php
@@ -1,20 +1,20 @@
 <footer>
     <div>
         <ul>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
+            <li> <a href="">Signaler un problème</a> </li>
+            <li> <a href="">Nous contacter</a> </li>
+            <li> <a href="">Devenir bénévole</a> </li>
         </ul>
         <ul>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
+            <li> <a href="">Mentions Légales</a> </li>
+            <li> <a href="">Conditions d'utilisation</a> </li>
         </ul>
         <ul>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
+            <li> Nos réseaux </li>
+            <li> <a href="">Facebook</a> </li>
+            <li> <a href="">Discord</a> </li>
+            <li> <a href="">YouTube</a> </li>
+            <li> <a href="">Instagram</a> </li>
         </ul>
     </div>
     <div>
