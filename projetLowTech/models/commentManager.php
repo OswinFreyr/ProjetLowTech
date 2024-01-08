eb86ab10af60ebb0a32b4d4393e88713f9ca35ad
ines <bouadilin@gmail.com>
2024-01-08 08:52:02 +0100

    managers

---
+++ projetLowTech/models/commentManager.php
@@ -0,0 +1,21 @@
+<?php
+
+class commentManager {
+    public function addComment($comment,$users_id,$posts_id) {
+        $comment = new Comment($comment,$users_id,$posts_id);
+        $comment->saveComment();
+        return "Votre commentaire a bien été enregistré";
+    }
+
+    public function likeComment($commentId) {
+        $comment = Comment::getCommentById($commentId);
+        $likes = $comment->getDetail("likes");
+        $likes+=1;
+        Comment::setDetail("likes",$likes,$commentId);
+        return "Liké";
+    }
+
+    public function replyToComment($commentId) {
+        // pour l'instant pas de réponse directe à un commentaire, ce sera comme une conv
+    }
+}
