da75214461e282015dd1033f41e3a8acd6422858
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-01-09 14:11:03 +0100

    test passage $_GET['search'] en variable dans annuaire

--- projetLowTech/controllers/annuaireController.php
+++ projetLowTech/controllers/annuaireController.php
@@ -5,11 +5,12 @@ require_once('./models/profilManager.php');
 $template = './views/pages/annuaire.php';
 
 $competencesPerUser = array();
+$search = $_GET['search'];
 
 if(empty($_GET)){
     $users = ProfilManager::getAllProfiles();
 }else if(!empty($_GET)){
-    $users = ProfilManager::getProfile($_GET['search']);
+    $users = ProfilManager::getProfile($search);
 }
 foreach($users as $user){
     $competencesPerUser[$user['id']] = ProfilManager::getCompetences($user['id']);
