a84a848b92dd7e8618015fb1513be54b320dc0fc
ines <bouadilin@gmail.com>
2024-01-10 11:10:33 +0100

    session

--- projetLowTech/views/pages/pageProfil.php
+++ projetLowTech/views/pages/pageProfil.php
@@ -1,3 +1,12 @@
+<?php
+// session_start();
+
+// if (!isset($_SESSION['utilisateur_connecte'])) {
+//     header("Location: Connexion.php");
+//     exit();
+// }
+?>
+
 <div>
     MENU (afaire)
 </div>
