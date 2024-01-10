0f3775342bac099c2b933dba0bf6c09a321e5188
ines <bouadilin@gmail.com>
2024-01-10 15:38:15 +0100

    gestion connexion

--- projetLowTech/controllers/connexionController.php
+++ projetLowTech/controllers/connexionController.php
@@ -1,52 +1,47 @@
 <?php
+require_once './models/connection.php';
 
 $template = './views/pages/Connexion.php';
-
+$message="";
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pseudo = $_POST['pseudo'];
     $password = $_POST['password'];
+    // var_dump($pseudo);
+    // var_dump($hashedMdp);
 
-    $servername = "localhost";
-    $username = "root";
-    $password_db = "";
-    $dbname = "dblowtech";
 
-    $connexion = new mysqli($servername, $username, $password_db, $dbname);
 
-    $sql = "SELECT id FROM users WHERE username = '$pseudo'";
-    $recupPseudo = $connexion->query($sql);
-
-    if ($connexion->connect_error) {
-        die("Connexion échouée: " . $connexion->connect_error);
-    }
-
-    $statement = $connexion->prepare("SELECT id, password FROM users WHERE username = ?");
-    $statement->bind_param("s", $pseudo);
+    $statement = dbConnect()->prepare("SELECT id,password FROM users WHERE username ='$pseudo'");
     $statement->execute();
-    $result = $statement->get_result();
+    $result = $statement->fetchAll();
 
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $hashedPassword = $row['password'];
-        if (password_verify($password, $hashedPassword)) {
-            $row = $recupPseudo->fetch_assoc();
-            $_SESSION['idUser'] = $row['id'];
-            header("Location: index.php?page=profil"); 
+    $verif = false;
 
-            exit;
-        } else {
-            ?>
-            <p>mot de passe invalide</p>
-            <?php
+    if (isset($result)) {
+        foreach ($result as $r){
+            
+            var_dump(password_verify($password,$r['password']));
+            if (password_verify($password, $r['password'])) {
+                $_SESSION['idUser'] = $r['id'] ;
+                $verif = true;
+                header("Location: index.php?page=profil"); 
+                exit;
+            }
+            
         }
+        if (!$verif){
+            $message = "Mot de passe invalide";
+        }
+        
     } else {
-        ?>
-            <p>Vous ne possédez pas de compte</p>
-            <?php
+
+           $message ="Vous ne possédez pas de compte";
+            
+        
     }
 
-    $statement->close();
-    $connexion->close();
+    // $statement->close();
+    // $connexion->close();
 }
--- projetLowTech/views/pages/Connexion.php
+++ projetLowTech/views/pages/Connexion.php
@@ -1,65 +1,17 @@
-<?php
-session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $pseudo = $_POST['pseudo'];
-    $password = $_POST['password'];
-
-    $servername = "localhost";
-    $username = "root";
-    $password_db = "";
-    $dbname = "dblowtech";
-
-    $connexion = new mysqli($servername, $username, $password_db, $dbname);
-
-    $sql = "SELECT id FROM users WHERE username = '$pseudo'";
-    $recupPseudo = $connexion->query($sql);
-
-    if ($connexion->connect_error) {
-        die("Connexion échouée: " . $connexion->connect_error);
-    }
-
-    $statement = $connexion->prepare("SELECT id, password FROM users WHERE username = ?");
-    $statement->bind_param("s", $pseudo);
-    $statement->execute();
-    $result = $statement->get_result();
-
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $hashedPassword = $row['password'];
-        if (password_verify($password, $hashedPassword)) {
-            $row = $recupPseudo->fetch_assoc();
-            $_SESSION['idUser'] = $row['id'];
-            header("Location: index.php?page=profil"); 
-
-            exit;
-        } else {
-            ?>
-            <p>mot de passe invalide</p>
-            <?php
-        }
-    } else {
-        ?>
-            <p>Vouas ne possédez pas de compte</p>
-            <?php
-    }
-
-    $statement->close();
-    $connexion->close();
-}
-?>
 
 
 <h2>Connexion</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+<form method="post" >
     Pseudo : <input type="text" name="pseudo"><br><br>
     Mot de passe : <input type="password" name="password"><br><br>
     <input type="submit" value="Se connecter">
 </form>
+<p><?= $message?></p>
 <div class="btnVersInscription">
     <h2>Vous ne possédez pas de compte ? </h2>
     <a href="index.php?page=inscription">Créer un compte</a>
 </div>
+
 
 
 
