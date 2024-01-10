45632a7fee5f00a5407b077284f6796f4c64f8c5
ines <bouadilin@gmail.com>
2024-01-10 13:54:51 +0100

    html dans isset

--- projetLowTech/controllers/profilController.php
+++ projetLowTech/controllers/profilController.php
@@ -9,10 +9,13 @@ $template = './views/pages/pageProfil.php';
 if (isset($_SESSION['idUser'])) {
     $user = ProfilManager::getProfileByID($_SESSION['idUser']);
     $competences = ProfilManager::getCompetences($user['2']);
+    var_dump("sessions");
     // 2 remplace id
     echo "Bonjour, ".$_SESSION['idUser']."!";
 } else {
     header("Location: index.php?page=connexion"); 
+    var_dump("pas sessions");
+    echo "pas sessions";
     exit();
 }
 
